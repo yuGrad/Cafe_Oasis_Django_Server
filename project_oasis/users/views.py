9a97652de827f8ec3fb91cbf7ed4d310d661ac06
iamdudumon <kksy4511@naver.com>
2023-09-22 17:42:13 +0900

    style: 사용하지 않는 import 제거

--- project_oasis/users/views.py
+++ project_oasis/users/views.py
@@ -1,11 +1,6 @@
-#from django.http            import JsonResponse  
-#from django.views           import View          
-#from django.core.exceptions import ValidationError
-#from django.db.models       import Q                                                                                                                
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
 
