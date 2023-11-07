ee29b673b593ad5dc61e11ef0fc6662b65760fd5
iamdudumon <kksy4511@naver.com>
2023-11-07 14:50:04 +0900

    fix: url route 수정/통합

--- project_oasis/users/urls.py
+++ project_oasis/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import LoginView, SignUpView, FindEmailView, FindPwView, EmailSendView
-from .views import EmailVerifyView, EditProfileView, CreateUserKeywords, UpdateUserKeywords
-from .views import isKeywordExist
+from .views import EmailVerifyView, ProfileView, UserKeyword
 
 urlpatterns = [
     path('login', LoginView.as_view()),
@@ -9,10 +8,8 @@ urlpatterns = [
     path('findemail', FindEmailView.as_view()),
     path('findpw', FindPwView.as_view()),
     path('mailsend', EmailSendView.as_view()),
-    path('mailverify',EmailVerifyView.as_view()),
-    path('profileEdit', EditProfileView.as_view()),
-    path('profile/keyword/create', CreateUserKeywords.as_view()),
-    path('profile/keyword/update', UpdateUserKeywords.as_view()),
-    path('profile/keyword/isexist', isKeywordExist.as_view()),
+    path('mailverify', EmailVerifyView.as_view()),
+    path('profile/', ProfileView.as_view()),
+    path('profile/keyword/', UserKeyword.as_view()),
     # path('cafeinfo', CafeInfoView.as_view()),
 ]
