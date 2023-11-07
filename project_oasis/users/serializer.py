0d94af63775664008ad452e5c8e54fa731ccd292
iamdudumon <kksy4511@naver.com>
2023-11-07 16:38:25 +0900

    feat: serializer를 통해 회원가입 기능 구현(crete 메소드 구현)

--- project_oasis/users/serializer.py
+++ project_oasis/users/serializer.py
@@ -1,27 +1,39 @@
 from rest_framework import serializers
 from .models import Customer, UserKeywords
 
+
 class CustomerSerializer(serializers.ModelSerializer):
-    class Meta: 
+    class Meta:
         model = Customer
-        fields = ('email', 'name', 'user_type', 'nickname',\
-                  'age', 'sex')
-        
+        fields = ('email', 'password', 'name', 'user_type', 'phone_no',
+                  'nickname', 'age', 'sex')
+
+    def create(self, validated_data):
+        return Customer.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            name=validated_data['name'],
+            # create_user 메소드는 extra_fields를 **kwargs로 받으므로,
+            # 나머지 필드는 extra_fields로 전달할 수 있습니다.
+            **{k: v for k, v in validated_data.items() if k not in ['email', 'password', 'name']}
+        )
+
+
 class UserKeywordsSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserKeywords
         fields = (
-            'beverage', 
-            'dessert', 
-            'various_menu', 
-            'special_menu', 
-            'large_store', 
-            'background', 
-            'talking', 
-            'concentration', 
-            'trendy_store', 
-            'gift_packaging', 
-            'parking', 
+            'beverage',
+            'dessert',
+            'various_menu',
+            'special_menu',
+            'large_store',
+            'background',
+            'talking',
+            'concentration',
+            'trendy_store',
+            'gift_packaging',
+            'parking',
             'price'
         )
 
