96eb467000f364ff850424d06f97a63b4b9d8ef6
iamdudumon <kksy4511@naver.com>
2023-11-12 05:24:33 +0900

    fix: /cafe로 endpoint 사용하기 위해 수정

--- project_oasis/cafe/urls.py
+++ project_oasis/cafe/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     #path('recommended', .as_view()),
-    path('recommend/keyword', views.recommend_cafes_base_keyword),
-    path('recommend/rating', views.recommend_cafes_base_rating),
+    path('', views.CafeView.as_view()),
+    path('/recommend/keyword', views.RecommendCafeKeywordView.as_view()),
+    path('/recommend/rating', views.RecommendCafeRatingView.as_view()),
+    
 ]
